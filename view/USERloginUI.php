0dd9d1b5062b3ba4a796ce1d10ab63dd899471cb
mdrshad97 <md.rshad97@gmail.com>
2024-04-20 11:52:03 +0800

    styles css

--- view/USERloginUI.php
+++ view/USERloginUI.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+    <link href="styles.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
@@ -31,16 +32,17 @@
     function displayLoginUI()
     {
     ?>
-        <form action="../view/USERloginUI.php" method="POST" class="row">
+    <div id="login">
+        <form action="../view/USERloginUI.php" method="POST" >
+            <h1>Log In</h1>
             <div class="col-auto">
                 <input type="hidden" id="flag" name="flag" value=1>
-                <label for="email" class="form-label">Email:</label><br>
-                <input class="form-control" type="email" id="email" name="email" placeholder="email@email.com"><br>
-                <label for="pw" class="form-label">Password:</label><br>
-                <input class="form-control" type="password" id="password" name="password">
-                <br>
-                <input class="form-control" type="submit" value="Submit">
+                <input class="form-control" type="email" id="email" name="email" placeholder="email">
+                <input class="form-control" type="password" id="password" name="password" placeholder="password">
+                <button>Sign In</button>
+                <!-- <input class="form-control" type="submit" value="Submit"> -->
             </div>
+    </div>
         <?php
     }
         ?>
